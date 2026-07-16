04dde343fe14da62b3d87109109fb4b4833503de
Henry Alvarez Astudillo <halvarezast@gmail.com>
2026-07-16 07:00:43 -0500

    Update Hallazgos

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -12,7 +12,7 @@
     .row.justify-content-center
       .col-lg-10.mb-5.bgs.p-4.brad
         figure
-          img(src="@/assets/curso/sintesis.svg", alt="Síntesis sobre  análisis, control y mejora continua de costos de operación que organiza procesos de comparación, verificación, registro de hallazgos y optimización de costos, junto con elementos como costos históricos, identificación de desviaciones, clasificación de costos, reportes, seguimiento de correcciones, análisis de variaciones y priorización de acciones.")
+          img(src="@/assets/curso/sintesis.svg", alt="Síntesis sobre análisis, control y mejora continua de costos de operación que organiza procesos de comparación, verificación, registro de hallazgos y optimización de costos, junto con elementos como costos históricos, identificación de desviaciones, clasificación de costos, reportes, seguimiento de correcciones, análisis de variaciones y priorización de acciones.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
